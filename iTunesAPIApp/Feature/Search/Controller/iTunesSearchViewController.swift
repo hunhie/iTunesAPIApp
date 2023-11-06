3e79a13ccab84e21adf78a6f87306c9762101ebc
walkerhilla <huny98.dev@gmail.com>
2023-11-06 17:08:43 +0900

    iTunesSearchViewController 생성 - 네비게이션 타이틀 설정 - 검색창 설정

---
+++ iTunesAPIApp/Feature/Search/Controller/iTunesSearchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  iTunesSearchViewController.swift
+//  iTunesAPIApp
+//
+//  Created by walkerhilla on 11/6/23.
+//
+
+import UIKit
+
+final class iTunesSearchViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    setnavigationItem()
+  }
+  
+  func setnavigationItem() {
+    let searchController = UISearchController(searchResultsController: nil)
+    searchController.searchBar.translatesAutoresizingMaskIntoConstraints = false
+    searchController.searchBar.placeholder = "게임, 앱, 스토리 등"
+    searchController.searchResultsUpdater = self
+    
+    navigationItem.title = "검색"
+    navigationItem.hidesSearchBarWhenScrolling = false
+    navigationItem.largeTitleDisplayMode = .always
+    navigationItem.searchController = searchController
+  }
+}
+
+extension iTunesSearchViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    
+  }
+}
