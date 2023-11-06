3eaea286aff20b1ea59a6b8a04a13fc614f4a6f0
walkerhilla <huny98.dev@gmail.com>
2023-11-06 17:07:52 +0900

    BaseViewController 생성 - 공통 설정이 적용된 뷰 컨트롤러 클래스

---
+++ iTunesAPIApp/Common/BaseViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseViewController.swift
+//  iTunesAPIApp
+//
+//  Created by walkerhilla on 11/6/23.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+  override func viewDidLoad() {
+    
+    view.backgroundColor = .systemBackground
+  }
+}
